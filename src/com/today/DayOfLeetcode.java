0cdee99354b440ee6e507c1006b1a80d1226f4a8
qwe1314229 <chenjiancxy@163.com>
2025-01-22 04:31:39 +0800

    windows10 测试2222

--- src/com/today/DayOfLeetcode.java
+++ src/com/today/DayOfLeetcode.java
@@ -6,7 +6,7 @@ import java.util.PriorityQueue;
 public class DayOfLeetcode {
 	public static void main(String[] args) {
 		//testMinimumCost();
-		System.out.println("windows7测试1111");
+		System.out.println("windows10 测试2222");
 	}
 
 	public static void testIsSubstringPresent() {
